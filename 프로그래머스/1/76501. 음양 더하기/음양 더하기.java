d392358d28342c6dca737fb175de56b78923cf97
kso9115 <148930264+kso9115@users.noreply.github.com>
2025-02-10 21:17:49 +0900

    [level 1] Title: 음양 더하기, Time: 0.08 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        
+        int sum=0;
+        
+        for(int i=0; i<absolutes.length; i++){
+	        if(signs[i] == false){
+	            absolutes[i] = absolutes[i]*(-1);			
+	        }
+	
+	    sum = sum + absolutes[i];
+        }
+        
+        return sum;
+    }
+}
