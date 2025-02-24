163a3618bcd323c049449aa68aa532d205b9729a
kso9115 <148930264+kso9115@users.noreply.github.com>
2025-02-24 21:39:45 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.22 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,28 +1,6 @@
-import java.util.*;
 class Solution {
     public int[] solution(int n, int m) {
-
-        List<Integer> listA = new ArrayList<>();
-        List<Integer> listB = new ArrayList<>();
-
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) {
-                listA.add(i);
-            }
-        }
-
-        for (int j = 1; j <= m; j++) {
-            if (m % j == 0) {
-                listB.add(j);
-            }
-        }
-
-        List<Integer> duplicates = new ArrayList<>(listA);
-        duplicates.retainAll(listB);
-
-        int max = Collections.max(duplicates);
-        int min = (n * m) / max;
-        
-        return new int[]{max, min};
+        int[] answer = {};
+        return answer;
     }
 }
